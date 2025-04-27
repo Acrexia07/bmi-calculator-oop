6d5c39330d0ad4ec3bca965fe3e0bf6361411ef8
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-04-27 19:27:21 +0800

    Reposition some variable declaration on the program

--- src/UserInputHandler.java
+++ src/UserInputHandler.java
@@ -4,6 +4,7 @@ public class UserInputHandler {
 
         // Initialization: Variable Declaration
         String name;
+        int age;
 
         // Process: Declare class for user input - Name
         do {
@@ -15,7 +16,7 @@ public class UserInputHandler {
         System.out.printf("Welcome to the BMI Calculator, %s!\n", name);
 
         // Process: Declare class for user input - Age
-        int age = InputValidator.validateAgeInput();
+        age = InputValidator.validateAgeInput();
 
         // Process: Create and return Person object with name and age
         return new Person(name, age, 0, 0);
